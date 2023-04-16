3a7065483fe5fe3ca34ae339f2345b1007a77a00
Zheleznykovich-Maxim <maxssmail572@gmail.com>
2023-04-16 19:58:17 +0300

    Initial commit

---
+++ src/Client2.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client2 {
+    private static final String SERVER_ADDRESS = "192.168.100.7";
+    private static final int SERVER_PORT = 8080;
+
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        out.println(name);
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while (true) {
+                        String message = in.readLine();
+                        System.out.println(message);
+                    }
+                } catch (IOException e) {
+                    System.out.println("Server connection closed");
+                }
+            }
+        });
+        thread.start();
+
+        while (true) {
+            String message = scanner.nextLine();
+            out.println(message);
+        }
+    }
+}
